937894d0e27c78d8570aafa32c1c31ab6535ded0
Jaynee De Claro <32161131+jayneejdc@users.noreply.github.com>
2017-12-15 11:30:59 +0800

    pages

---
+++ error.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Error</title>
+    <link rel="stylesheet" type="text/css" href="la/la.css">
+    <link rel="stylesheet" type="text/css" href="la/hays.css">
+</head>
+<body>
+<div class="form">
+    <h2>Error pls contact our administration. Your account has been blocked.</h2>   
+    <a href="index.php"><button class="button button-block"/>Home</button></a>
+</div>
+</body>
+</html>
